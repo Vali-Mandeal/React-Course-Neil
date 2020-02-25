183050536e9693ff6f35acc2154ce5c252c2259b
Vali-Mandeal <51697555+Vali-Mandeal@users.noreply.github.com>
2020-02-25 21:19:23 +0200

    3.21 Building the first component

--- revents/src/app/layout/App.jsx
+++ revents/src/app/layout/App.jsx
@@ -1,17 +1,13 @@
 import React, { Component } from "react";
-import { Button } from 'semantic-ui-react';
+
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Re-events</h1>
-        <button className="ui icon button">
-          <i className="smile icon"></i>
-          CSS BUTTON
-        </button>
-
-        <Button icon="smile" content="React Button"></Button>
+        <EventDashboard/>
         
       </div>
     );
