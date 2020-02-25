b337d8c16547b033c11a4a91dd5a30c98bdb1510
Vali-Mandeal <51697555+Vali-Mandeal@users.noreply.github.com>
2020-02-25 21:09:23 +0200

    .

--- revents/src/app/layout/App.jsx
+++ revents/src/app/layout/App.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
-import { Button } from "semantic-ui-react";
+import { Button } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Re-events</h1>
-        <Button icon='smile' content='React' />
+        <button className="ui icon button">
+          <i className="smile icon"></i>
+          CSS BUTTON
+        </button>
+
+        <Button icon="smile" content="React Button"></Button>
+        
       </div>
     );
   }
