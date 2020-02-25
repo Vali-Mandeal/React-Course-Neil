e7b9fee726b2cc63b4a1bd30d657530ee0b32bfd
Vali-Mandeal <51697555+Vali-Mandeal@users.noreply.github.com>
2020-02-25 23:22:57 +0200

    3.28: Inverse data flow

--- revents/src/features/event/EventDashboard/EventDashboard.jsx
+++ revents/src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,75 +1,39 @@
 import React, { Component } from "react";
-import { Grid, Button } from 'semantic-ui-react'
+import { Grid, Button } from "semantic-ui-react";
 
-import EventList from '../../event/EventList/EventList'
-import EventForm from '../../event/EventForm/EventForm'
+import EventList from "../../event/EventList/EventList";
+import EventForm from "../../event/EventForm/EventForm";
 
-const eventsFromDashBoard = [
-  {
-    id: '1',
-    title: 'Trip to Tower of London',
-    date: '2018-03-27T11:00:00+00:00',
-    category: 'culture',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
-    city: 'London, UK',
-    venue: "Tower of London, St Katharine's & Wapping, London",
-    hostedBy: 'Bob',
-    hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
-    attendees: [
-      {
-        id: 'a',
-        name: 'Bob',
-        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
-      },
-      {
-        id: 'b',
-        name: 'Tom',
-        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
-      }
-    ]
-  },
-  {
-    id: '2',
-    title: 'Trip to Punch and Judy Pub',
-    date: '2018-03-28T14:00:00+00:00',
-    category: 'drinks',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
-    city: 'London, UK',
-    venue: 'Punch & Judy, Henrietta Street, London, UK',
-    hostedBy: 'Tom',
-    hostPhotoURL: 'https://randomuser.me/api/portraits/men/22.jpg',
-    attendees: [
-      {
-        id: 'b',
-        name: 'Tom',
-        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
-      },
-      {
-        id: 'a',
-        name: 'Bob',
-        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
-      }
-    ]
-  }
-]
-
+import seedData from "./seedData";
 
 class EventDashboard extends Component {
+  state = {
+    events: seedData,
+    isOpen: false
+  };
+
+  handleIsOpenToggle = () => {
+    this.setState(({isOpen}) => ({
+      isOpen: !isOpen
+    }));
+  };
 
   render() {
-    return <div>
+    const { events, isOpen } = this.state;
+
+    return (
+      <div>
         <Grid>
-            <Grid.Column width={10}>
-                <EventList events={eventsFromDashBoard}/>
-            </Grid.Column>
-            <Grid.Column width={6}>
-              <Button positive content='Create Event'/>
-                <EventForm/>
-            </Grid.Column>
+          <Grid.Column width={10}>
+            <EventList events={events} />
+          </Grid.Column>
+          <Grid.Column width={6}>
+            <Button onClick={this.handleIsOpenToggle} positive content='Create Event' />
+            {isOpen && <EventForm cancelFormOpen={this.handleIsOpenToggle}/>}
+          </Grid.Column>
         </Grid>
-    </div>;
+      </div>
+    );
   }
 }
 
