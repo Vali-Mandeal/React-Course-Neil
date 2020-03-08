62d5c0f21e164906a07e0907129d95c35cc0abad
Vali-Mandeal <51697555+Vali-Mandeal@users.noreply.github.com>
2020-03-08 16:17:14 +0200

    9.75: Redux Forms setup

--- revents/src/features/events/EventForm/EventForm.jsx
+++ revents/src/features/events/EventForm/EventForm.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-
+import {reduxForm, Field} from "redux-form";
 import { Segment, Form, Button } from "semantic-ui-react";
-
 import cuid from "cuid";
 
 import { createEvent, updateEvent } from "../eventActions";
+import { formValueSelector } from "redux-form";
 
 const mapState = (state, ownProps) => {
   const eventId = ownProps.match.params.id;
@@ -76,15 +76,7 @@ class EventForm extends Component {
     return (
       <Segment>
         <Form onSubmit={this.handleFormSubmit} autoComplete='off'>
-          <Form.Field>
-            <label>Event Title</label>
-            <input
-              name='title'
-              onChange={this.handleInputChange}
-              value={title}
-              placeholder='Event Title'
-            />
-          </Form.Field>
+         <Field name='title' component='input' placeholder='Event title'/>
           <Form.Field>
             <label>Event Date</label>
             <input
@@ -134,4 +126,4 @@ class EventForm extends Component {
   }
 }
 
-export default connect(mapState, actions)(EventForm);
+export default connect(mapState, actions)(reduxForm({form: 'eventForm'})(EventForm));
